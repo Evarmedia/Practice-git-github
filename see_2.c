852581800ee47f0ea87b9a99451b92c855fbca94
Mishak Mosimabale <37378747+Evarmedia@users.noreply.github.com>
2022-08-17 17:30:42 +0100

    Update see_2.c

--- see_2.c
+++ see_2.c
@@ -1,4 +1,23 @@
 #include <stdio.h> 
 int main(){
-	
+	int a; 
+	float f; 
+	double d;
+	char c; 
+
+	scanf("%d", &a); 
+	printf("You entered: %d\n", a); 
+
+	scanf("%f", &f); 
+	printf("You entered: %f\n", f); 
+
+	scanf("%lf", &d); 
+	printf("You entered: %f\n", d); 
+
+	fflush(stdin); 
+
+	scanf("%c", &c); 
+	printf("You entered: %c", c);
+
+	return 0;
 }
